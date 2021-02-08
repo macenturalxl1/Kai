59d66ac1ba4ea5a0e4ab091fd2268bff3a6d57f5
K-itKat20 <LaurelKatie@tutanota.com>
2021-02-08 12:13:41 +0000

    Changes to config.js to work with local

--- ui/src/rest/config.ts
+++ ui/src/rest/config.ts
@@ -6,4 +6,13 @@ export class Config {
     static REACT_APP_COGNITO_USERPOOLID = _env.REACT_APP_COGNITO_USERPOOLID;
     static REACT_APP_COGNITO_CLIENTID = _env.REACT_APP_COGNITO_CLIENTID;
     static REACT_APP_AUTH_ENDPOINT = _env.REACT_APP_AUTH_ENDPOINT;
+    //To use locally, uncomment below and comment above
+    // static REACT_APP_API_PLATFORM = 'test'
+    // static REACT_APP_KAI_REST_API_HOST = 'http://localhost:4000';
+    // static REACT_APP_COGNITO_USERPOOLID = _env.REACT_APP_COGNITO_USERPOOLID;
+    // static REACT_APP_COGNITO_CLIENTID = _env.REACT_APP_COGNITO_CLIENTID;
+    // static REACT_APP_AUTH_ENDPOINT = 'http://localhost:4000';
+
 }
+
+
