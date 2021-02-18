c5e59713a38714198e1a6f33e61f1f0a78d3c4b2
Rocky <rocky341@protonmail.com>
2021-02-18 16:33:07 +0000

    changed delete graph expected response from 202 to 204

--- ui/src/rest/repositories/delete-graph-repo.ts
+++ ui/src/rest/repositories/delete-graph-repo.ts
@@ -4,7 +4,7 @@ export class DeleteGraphRepo {
     public async delete(graphId: string): Promise<void> {
         const response: IApiResponse<undefined> = await RestClient.delete(graphId);
 
-        if (response.status !== 202) {
+        if (response.status !== 204) {
             throw new Error(`Expected status code 202 for Accepted Delete Graph Process but got (${response.status})`);
         }
     }
